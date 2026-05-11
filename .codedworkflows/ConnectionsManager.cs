02a931f17db1d52ebf595d478b8b3e87a5f76f5f
nitinsarvarth <nitin.chauhan@sarvarth.com>
2026-05-12 02:51:14 +0530

    first commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace UiPathDemo
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
